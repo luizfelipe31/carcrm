11a71133e1cd61a5ef2f4d2874c071ddf9835d56
luizf <luizf@LAPTOP-SOCDOATH>
2021-04-30 21:14:50 -0300

    vehicles

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\api\VehiclesController;
+use App\Http\Controllers\webservice\WebserviceController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::apiResources([
+      'vehicles' => VehiclesController::class
+]);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('vehicles/{vehicle_type}/brand', [VehiclesController::class, 'brand']);
+Route::get('vehicles/{vehicle_type}/{vehicle_brand}/model', [VehiclesController::class, 'model']);
+Route::get('vehicles/{vehicle_brand}/{vehicle_model}/version', [VehiclesController::class, 'version']);
+
+Route::group(['prefix' => 'webservice'], function(){
+      Route::post('cep', [WebserviceController::class, 'cep']);
 });
